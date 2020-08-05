437ebc6790405a089099e7f96175e86dafe9c648
monkey0525 <69131612+monkey0525@users.noreply.github.com>
2020-08-05 11:40:31 +0800

    Add files via upload

---
+++ day3-2.py
@@ -0,0 +1,13 @@
+import turtle
+fu=turtle.Turtle()
+ck=turtle.Screen()
+ck.bgcolor("white")
+fu.color("black")
+fu.shape("turtle")  
+def functionname (callshit):
+    fu.forward(100)
+    fu.left(90)
+    fu.forward(100)
+    fu.left(90)
+    fu.forward(100)
+    fu.left(90)
